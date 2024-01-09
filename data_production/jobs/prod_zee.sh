70c3fa613172c0b42e3bc5002810ead45c123f2d
Edmar De Souza <edesouza@lpnlp100-3.local>
2024-01-09 23:42:17 +0100

    update scriot to data production

--- data_production/jobs/prod_zee.sh
+++ data_production/jobs/prod_zee.sh
@@ -1,21 +1,19 @@
-CPU_N=$(grep -c ^processor /proc/cpuinfo)
+
 CPU_N=2
 
+mkdir EVT && cd EVT
 prun_evts.py -c "gen_zee.py" -nt $CPU_N -o zee.EVT.root --nov 500 --novPerJob 100 -m
 
 cd ../
 mkdir HIT && cd HIT
-
 simu_trf.py -i ../EVT/zee.EVT.root -nt $CPU_N -o zee.HIT.root
 
 cd ../
 mkdir ESD && cd ESD
-
 digit_trf.py -i ../HIT/* -o zee.ESD.root
 
 cd ../
 mkdir AOD && cd AOD
-
 reco_trf.py -i ../ESD/* -o zee.AOD.root
 
 cd ../
